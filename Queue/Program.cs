90339eec52e906134b983ace530bcb836529a81d
Lucilary <gofafafa1@gmail.com>
2023-10-05 15:43:16 +0700

    Update after Class lastest

---
+++ Queue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue q = new LinkedListPriorityQueue();
+            q.enqueue(1);
+            q.enqueue(2);
+            q.enqueue(3);
+            q.enqueue(4);
+            q.enqueue(5);
+            Console.WriteLine(q.dequeue());
+            Console.WriteLine(q.dequeue());
+            Console.WriteLine(q.dequeue());
+        }
+    }
+}
---
+++ Queue/Queuecs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public interface Queuecs
+    {
+        bool isEmpty();
+        int size();
+        void enqueue(object e);
+        object dequeue();
+        object peek();
+    }
+}
